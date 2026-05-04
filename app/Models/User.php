79359c6c9fb863ac42503c78628a0e683a5f8dd5
sokcheadalinsorn <sokcheadalinsorn@gmail.com>
2026-05-04 16:15:37 +0700

    create/migrate done

--- app/Models/User.php
+++ app/Models/User.php
@@ -29,4 +29,10 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+     // Relationship one users has many tasks
+    public function tasks()
+    {
+        return $this->HasMany(Task::class);
+    }
 }
