76115c606fa77ec0b7aac323fb9e637a23c59837
sokcheadalinsorn <sokcheadalinsorn@gmail.com>
2026-05-06 13:54:19 +0700

    create/migrate

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
@@ -10,3 +11,9 @@ Route::get('/', function () {
 Route::get('/tasks', [TaskController::class, 'index']);
 Route::post('/tasks', [TaskController::class, 'store'])->name('tasks.store');
 Route::get('/tasks', [TaskController::class, 'show']);
+
+Route::get('/login', [AuthController::class, 'showLogin']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout']);
+Route::post('/login', [ AuthController::class, 'store'])->name('login.store');
+
